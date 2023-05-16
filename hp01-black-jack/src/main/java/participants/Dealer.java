af71b545523ca0ee5a06f9d8cc67a830b22d3ce5
holczur <holcz.norbert@gmail.com>
2023-05-16 09:01:27 +0200

    Dealer class tested

---
+++ hp01-black-jack/src/main/java/participants/Dealer.java
@@ -0,0 +1,32 @@
+package participants;
+
+import cards.Card;
+import cards.Deck;
+import engine.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dealer extends Participant {
+    private Deck deck;
+    private List<Card> pile;
+
+    public Dealer() {
+        super("DEALER");
+        super.coins = Table.DEALER_COINS;
+        this.deck = new Deck();
+        this.pile = new ArrayList<>();
+    }
+
+    public void deal(Participant player) {
+        player.hand.add(deck.getDeck().get(0));
+    }
+
+    public void deal(List<Participant> players) {
+        for (Participant player : players) {
+            player.hand.add(deck.getDeck().get(0));
+        }
+    }
+
+
+}
---
+++ hp01-black-jack/src/test/participants/DealerTest.java
@@ -0,0 +1,38 @@
+package participants;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerTest {
+    private List<Participant> players;
+    private Participant aladar = new Player("Aladár");
+    private Participant bela = new Player("Béla");
+    private Dealer dealer;
+
+    @BeforeEach
+    void init(){
+        dealer = new Dealer();
+        players = new ArrayList<>();
+        players.add(aladar);
+        players.add(bela);
+    }
+
+    @Test
+    void test_deal_oneCardToOnePlayer() {
+        dealer.deal(aladar);
+        assertEquals(1,aladar.hand.size());
+    }
+
+    @Test
+    void test_deal_oneCardToAllPlayers() {
+        dealer.deal(players);
+        assertEquals(2,aladar.hand.size() + bela.hand.size());
+    }
+
+}
