17b2ae1606e6bdfee0019190185b38e17d0579f4
holczur <holcz.norbert@gmail.com>
2023-05-16 09:54:11 +0200

    Player class is ready with unfinished methods

---
+++ hp01-black-jack/src/main/java/participants/Player.java
@@ -0,0 +1,15 @@
+package participants;
+
+import engine.Table;
+
+public class Player extends Participant{
+
+
+    public Player(String name, Table table) {
+        super(name, table);
+    }
+
+    public void split(){}
+
+    public void insure(){}
+}
